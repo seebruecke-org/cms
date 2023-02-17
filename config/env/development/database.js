ee60c168f2c2260f2ea9ec456ae53970abefed5f
Azin <aazadi@gmail.com>
2023-02-17 20:28:46 +0100

    chore: line breaks

--- config/env/development/database.js
+++ config/env/development/database.js
@@ -7,9 +7,9 @@ module.exports = ({ env }) => ({
         client: 'mysql',
         host: env('DATABASE_HOST', 'localhost'),
         port: env.int('DATABASE_PORT', 3306),
-        database: env('DATABASE_NAME', 'sb'),
+        database: env('DATABASE_NAME', 'sbfull'),
         username: env('DATABASE_USERNAME', 'root'),
-        password: env('DATABASE_PASSWORD', ''),
+        password: env('DATABASE_PASSWORD', 'root'),
         charset: 'utf8mb4',
       },
       options: {
