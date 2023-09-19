b495818d73f90949cdd618a8f5d30c9740ae1579
Azin <aazadi@gmail.com>
2023-09-19 11:13:23 +0200

    Autogenerate Schema.graphql and types.d.ts

--- config/plugins.js
+++ config/plugins.js
@@ -1,8 +1,15 @@
+const { join } = require('path');
+
 module.exports = {
   'duplicate-button': true,
   graphql: {
     config: {
       defaultLimit: 1000,
+      generateArtifacts: true,
+      artifacts: {
+        schema: join(__dirname, '..', 'exports', 'graphql', 'schema.graphql'),
+        typegen: join(__dirname, '..', 'exports', 'graphql', 'types.d.ts'),
+      },
     },
   },
 };
