5fc96e84992438f36f164d80e838a0822305d0dc
Gregor <apriljunge@gmail.com>
2025-11-16 12:15:18 +0100

    Revert "Add possibility for local sqlite developement"
    This reverts commit 24268a19631bac3d7513c15f1f3a3aab84cddfe5.

--- config/database.js
+++ config/database.js
@@ -1,32 +1,15 @@
-const path = require('path');
-
-module.exports = ({ env }) => {
-  if (env("LOCAL_DEV", false)) {
-    return ({
-      connection: {
-        client: 'sqlite',
-        connection: {
-          filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
-        },
-        useNullAsDefault: true,
-      },
-    })
-  }
-
-  return ({
-    defaultConnection: 'default',
-    connections: {
-      default: {
-        connector: 'bookshelf',
-        settings: {
-          client: 'sqlite',
-          filename: env('DATABASE_FILENAME', '.tmp/data.db'),
-        },
-        options: {
-          useNullAsDefault: true,
-        },
-      },
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'mysql',
+    connection: {
+      host: env('DATABASE_HOST', 'localhost'),
+      port: env.int('DATABASE_PORT', 3306),
+      database: env('DATABASE_NAME', 'strapi'),
+      user: env('DATABASE_USERNAME', 'strapi'),
+      password: env('DATABASE_PASSWORD', 'strapi'),
+      charset: 'utf8mb4',
+      ssl: env('DATABASE_SSL', false),
     },
-  });
-}
-  
+    debug: false,
+  },
+});
